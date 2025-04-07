666765e4474a4aa3916ceb852b725fa285e4224f
LucioWi <luciowiesek@gmail.com>
2025-04-07 11:15:08 -0300

    Actividad 2 agregada

---
+++ Actividad2Silo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Actividad2Silo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Silo silo = new Silo();
+
+            Console.Write("Ingrese el radio del silo (en metros): ");
+            silo.Radio = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingrese la altura del silo (en metros): ");
+            silo.Altura = Convert.ToDouble(Console.ReadLine());
+
+            double volumen = silo.CalcularVolumen();
+            Console.WriteLine("El volumen máximo del silo es: " + volumen + " m³");
+        }
+    }
+}
---
+++ Actividad2Silo/Silo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad2Silo
+{
+    public class Silo
+    {
+        private double radio;
+        private double altura;
+
+        public double Radio
+        {
+            get { return radio; }
+            set { radio = value; }
+        }
+
+        public double Altura
+        {
+            get { return altura; }
+            set { altura = value; }
+        }
+
+        public double CalcularVolumen()
+        {
+            return Math.PI * Math.Pow(radio, 2) * altura;
+        }
+    }
+}
