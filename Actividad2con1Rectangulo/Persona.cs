cd74b66b17c5e0ccc7a5b40b8e13e64925bfc774
alumno <alumno@GI.FRC.UTN.EDU.AR>
2025-04-07 15:59:17 -0300

    Actividad 2.4 Persona

---
+++ Actividad2con1Rectangulo/Persona.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad2con4Persona
+{
+    public class Persona
+    {
+        private string nombre = "";
+        private int edad = 0;
+        private char sexo = '0';
+        private double peso= 0 , altura = 0;
+
+        public string Nombre
+        {
+            get { return nombre; } //propiedades
+            set { nombre = value; } //propiedades
+        }
+        public int Edad
+        {
+            get { return edad; } //propiedades
+            set { edad = value; } //propiedades
+        }
+        public char Sexo
+        {
+            get { return sexo; } //propiedades
+            set { sexo = value; } //propiedades
+        }
+        public double Peso
+        {
+            get { return peso; } //propiedades
+            set { peso = value; } //propiedades
+        }
+        public double Altura
+        {
+            get { return altura; } //propiedades
+            set { altura = value; } //propiedades
+        }
+
+        public double calcularIMC()
+        {
+            double pesoIdeal = 0;
+            double IMCfinal = 0;
+            pesoIdeal = peso / (altura * altura);
+
+            if (pesoIdeal < 20)
+            {
+                IMCfinal = -1;
+            }
+
+            if (pesoIdeal >= 20 & pesoIdeal <= 25)
+            {
+                IMCfinal = 0;
+            }
+
+            if (pesoIdeal > 25)
+            {
+                IMCfinal = 1;
+            }
+
+            return IMCfinal;
+        }
+
+        public Boolean esMayorDeEdad()
+        {
+            Boolean esMayorDeEdad;
+            if (edad >= 21)
+            {
+                esMayorDeEdad = true;
+            } else
+            {
+                esMayorDeEdad = false;
+            }
+
+            return esMayorDeEdad;
+        }
+    }
+}
---
+++ Actividad2con1Rectangulo/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+ Persona. Se nos pide modelar una clase Persona con las siguientes
+condiciones:
+▪ Sus atributos son: nombre, edad, sexo (H hombre, M mujer), peso y altura.
+▪ Los métodos que se implementarán son:
+o calcularIMC(): calculará si la persona está en su peso ideal (peso en
+kg/(altura^2 en m)), si esta fórmula devuelve un valor menor que 20, la
+función devuelve un -1, si devuelve un número entre 20 y 25 (incluidos),
+significa que está por debajo de su peso ideal la función devuelve un 0
+y si devuelve un valor mayor que 25 significa que tiene sobrepeso, la
+función devuelve un 1. Se recomienda usar constantes para devolver
+estos valores.
+o esMayorDeEdad(): indica si es mayor de edad(21 años), devuelve un
+booleano. 
+ */
+
+using System;
+
+namespace Actividad2con4Persona
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona per1= new Persona();
+
+            Console.Write("Ingrese el nombre: ");
+            per1.Nombre = Convert.ToString(Console.ReadLine());
+
+            Console.Write("Ingrese la edad: ");
+            per1.Edad = Convert.ToInt16(Console.ReadLine());
+
+            Console.Write("Ingrese el sexo (M = Masculino / F = Femenino): ");
+            per1.Sexo = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Ingrese el peso (En Kg): ");
+            per1.Peso = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingrese la altura (En m / Ejemplo: '1,80'): ");
+            per1.Altura = Convert.ToDouble(Console.ReadLine());
+
+            double IMC = per1.calcularIMC();
+            Boolean esMayor = per1.esMayorDeEdad();
+
+            Console.WriteLine("-1 = Esta en su indice corporal. / 0 = Esta por debajo de su indice corporal. / 1 = Esta por encima de su indice corporal.");
+            Console.WriteLine(" ");
+            Console.WriteLine("Su indice de masa corporal es: " + IMC);
+            Console.WriteLine("¿Es mayor de edad?: " + esMayor);
+        }
+    }
+}
