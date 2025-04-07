6da8ea451b045a85488fdd8b88b5875754af9e77
LucioWi <luciowiesek@gmail.com>
2025-04-07 11:03:55 -0300

    probando

---
+++ Actividad1Rectangulo/Actividad1Rectangulo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Actividad1Rectangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crear un rectángulo con base 3 y altura 5
+            Rectangulo rect = new Rectangulo();
+            rect.Ancho = 3;
+            rect.Alto = 5;
+
+            // Calcular resultados
+            int perimetro = rect.CalcularPer();
+            int superficie = rect.CalcularSup();
+
+            // Mostrar resultados
+            Console.WriteLine("La base del rectangulo es: " + rect.Ancho);
+            Console.WriteLine("La altura del rectangulo es: " + rect.Alto);
+            Console.WriteLine("El perímetro del rectangulo es: " + perimetro);
+            Console.WriteLine("La superficie del rectangulo es: " + superficie);
+        }
+    }
+}
---
+++ Actividad1Rectangulo/Actividad1Rectangulo/Rectangulo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad1Rectangulo
+{
+    public class Rectangulo
+    {
+        private int ancho;
+        private int alto;
+
+        public int Ancho
+        {
+            get { return ancho; }
+            set { ancho = value; }
+        }
+        public int Alto
+        {
+            get { return alto; }
+            set { alto = value; }
+        }
+
+        public int CalcularPer()
+        {
+            return 2 * (Ancho + Alto);
+        }
+
+        public int CalcularSup()
+        {
+            return Ancho * Alto;
+        }
+    }
+}
